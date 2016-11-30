b03955a40a6d42617703443e7dff17002536bf7b
Santtu <santtu.mh@gmail.com>
2016-11-30 05:16:14 +0200

    -Muutetaan pallo RigidBodyksi

--- pallo.gd
+++ pallo.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 const PALLON_NOPEUS = 4
 const PELIALUE_X = 1010
@@ -13,15 +13,19 @@ var pallo_suunta = Vector2(2, PALLON_NOPEUS)
 var supervoima = false
 
 func _ready():
+	#set_use_custom_integrator(true)
 	set_fixed_process(true)
 
+func _integrate_forces(state):
+	set_linear_velocity(pallo_suunta.normalized()*state*PALLON_NOPEUS)
+	
 func _fixed_process(delta):
 	
 	if (is_colliding()):
 		
 		var tormaaja = get_collider()
 		
-		if (tormaaja.get_name() == "lauta"):
+		if (tormaaja == lauta):
 			var osuu_kohtaan = (get_pos().x - lauta.get_pos().x) / 10
 			pallo_suunta = Vector2(osuu_kohtaan, -PALLON_NOPEUS)
 			soundit.play("blomp2")
